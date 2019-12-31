a0a84c96a38fb73af7b990cbcc7fd87c90f73ee7
randolrs <r.shane.randolph@gmail.com>
2019-12-31 12:52:47 -0800

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # devise_for :users, controllers: {sessions: "sessions"}
+
+  get '/logged_in', to: 'sessions#is_logged_in?'
+
+  namespace :api, :defaults => {:format => :json} do
+    namespace :v1 do
+      post '/login', to: 'sessions#create'
+      delete '/logout', to: 'sessions#destroy'
+      get '/logged_in', to: 'sessions#is_logged_in?'
+      resources :users, only: [:create, :show, :index]
+
+      # get 'logged_in', to: 'users#logged_in?'
+    end
+  end
+end
